c934396de1862e1dc3f4a6c9401d105ffb9af2af
ka0824 <rkgud0824@gmail.com>
2022-03-07 17:34:18 +0900

    [Server]connect mysql

--- login-server/app.js
+++ login-server/app.js
@@ -1,10 +1,23 @@
 import express from "express";
+import mysql from "mysql";
 
 const app = express();
 
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: "",
+    database: "login_server"
+});
+
+con.connect(function (err) {
+    if (err) throw err;
+    console.log('connected');
+})
+
 app.get("/", (req, res) => {
     res.send("서버에 연결 되었습니다.")
-})
+});
 
 app.listen(3001, () => {
     console.log("서버 연결 완료")

cd751fa028826c1b4ab540ada7cd6b7c4b5a9bd8
ka0824 <rkgud0824@gmail.com>
2022-03-08 10:33:32 +0900

    [Server]add cors

--- login-server/app.js
+++ login-server/app.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const userRouter = require('./routers/userRouter');
+const cors = require('cors');
 
 
 const app = express();
 app.use(express.json());
+
+app.use(
+    cors({
+        orgin: ['http://localhost:3000'],
+        method:['GET', 'POST', 'DELETE', 'PUT', 'PATCH']
+    })
+)
 
 app.get("/", (req, res) => {
     res.send({message: "success!"})

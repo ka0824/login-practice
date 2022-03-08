d09298e7bd51a3de5b14cb2b15b6e3cee462b7c8
ka0824 <rkgud0824@gmail.com>
2022-03-08 14:14:31 +0900

    [Client]add login

--- login-client/src/App.js
+++ login-client/src/App.js
@@ -9,10 +9,35 @@ import '../src/css/App.css';
 function App() {
 
   const [userInfo, setUserInfo] = useState({});
+  const [input, setInput] = useState({
+    id: "",
+    password: ""
+  })
   const navigate = useNavigate();
 
   const routeHandler = () => {
     navigate("/signup")
+  }
+
+  const getUser = async (userInfo) => {
+    const { id, password } = userInfo;
+
+    try {
+
+      const response = await axios.post('http://localhost:3001/user/signin', {
+        id: id,
+        password: password
+      })
+      navigate('/loggined');
+    } catch (err) {
+      const status = err.response.status;
+      if (status === 403) {
+        alert('입력한 정보를 다시 한번 확인해 주세요.');
+      } else {
+        alert('서버에 문제가 발생했습니다. 잠시만 기다려주세요');
+      }
+    }
+    
   }
 
   return (
@@ -23,14 +48,14 @@ function App() {
               <div className="login-box">
                 <div className="id-box">
                   <div className="texts">Id</div>
-                  <input></input>
+                  <input onBlur={(e) => setInput({ ...input, id: e.target.value})}></input>
                 </div>
                 <div className="password-box">
                   <div className="texts">Password</div>
-                  <input></input>
+                  <input onBlur={(e) => setInput({ ...input, password: e.target.value})}></input>
                 </div>
                 <div className="buttons">
-                  <div className="buttons-text">로그인</div>
+                  <div className="buttons-text" onClick={() => getUser(input)}>로그인</div>
                   <div className="buttons-text" onClick={routeHandler}>회원가입</div>
                 </div>
               </div>
